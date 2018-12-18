ccbc9abd854b65cf0f306750a797d34b21e246ca
godline <g132.2012@gmail.com>
2018-12-18 10:29:58 +0100

    This is origianl index.html created

---
+++ jscode.js
@@ -0,0 +1,25 @@
+function switchOff(){
+
+       document.getElementById("black").src="bulbs/black_out.jpg";
+
+}
+
+
+
+
+function changeToBlue(){
+
+	
+        document.getElementById("black").src="bulbs/blue_light.jpg";
+} 
+function changeToRed(){
+
+	document.getElementById("black").src="bulbs/red_light.jpg";
+
+}
+function changeToYellow(){
+
+	document.getElementById("black").src="bulbs/yello_light.jpg";
+
+}
+
